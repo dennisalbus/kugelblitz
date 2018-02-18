82393ae620ff6110aa69f1a8feeb5e7f3b0232ca
Dennis Albus <code@dennisalbus.com>
2018-02-18 14:30:33 +0100

    Minor refactoring

--- src/com/dennisalbus/kugelblitz/kugelblitz.kt
+++ src/com/dennisalbus/kugelblitz/kugelblitz.kt
@@ -4,23 +4,28 @@ fun hit_sphere(center: Vec3, radius: Double, ray: Ray): Boolean {
     val oc = ray.origin - center
     val a = dot(ray.direction, ray.direction)
     val b = 2.0 * dot(oc, ray.direction)
-    val c= dot(oc, oc) - radius*radius
-    val discriminant = b*b - 4.0*a*c
+    val c = dot(oc, oc) - radius * radius
+    val discriminant = b * b - 4.0 * a * c
     return (discriminant > 0)
 }
 
 fun color(ray: Ray): Vec3 {
-    if (hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray)) {
-        return Vec3(1.0, 0.0, 0.0)
+    return if (hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, ray)) {
+        Vec3(1.0, 0.0, 0.0)
+    } else {
+        background(ray)
     }
+}
+
+fun background(ray: Ray): Vec3 {
     val unitDirection = ray.direction.normalized()
     val t = 0.5 * unitDirection.y + 1.0
     return lerp(Vec3(1.0, 1.0, 1.0), Vec3(0.5, 0.7, 1.0), t)
 }
 
 fun main(args: Array<String>) {
-    val xres = 200
-    val yres = 100
+    val xres = 800
+    val yres = 400
 
     val lowerLeftCorner = Vec3(-2.0, -1.0, -1.0)
     val horizontal = Vec3(4.0, 0.0, 0.0)
